75c8fc0a38d5162cd4f5ce25312ead20d8ae4fed
Bhavana-s-iiitb <bhavanas284@gmail.com>
2024-08-22 00:29:48 +0530

    Create sequential_calculator.v

---
+++ RISC-V MYTH Workshop/Codes/sequential_calculator.v
@@ -0,0 +1,36 @@
+\m5_TLV_version 1d: tl-x.org
+\m5
+   
+   // =================================================
+   // Welcome!  New to Makerchip? Try the "Learn" menu.
+   // =================================================
+   
+   //use(m5-1.0)   /// uncomment to use M5 macro library.
+\SV
+   // Macro providing required top-level module definition, random
+   // stimulus support, and Verilator config.
+   m5_makerchip_module   // (Expanded in Nav-TLV pane.)
+	
+\TLV
+   //$count[31:0] = 32'b0;
+   
+   // Sequential Clock
+   
+   |calc
+      @1
+         $clk_tan = *clk;
+         $reset = *reset;
+         $val1[31:0] = >>1$result[31:0];
+         $val2[31:0] = $rand2[3:0];
+         $result[31:0] = $reset ? 32'b0 : ($sel[1:0] == 2'b00)
+                         ? ($val1[31:0] + $val2[31:0]) : ($sel[1:0] == 2'b01)
+                         ? ($val1[31:0] - $val2[31:0]) : ($sel[1:0] == 2'b10)
+                         ? ($val1[31:0] * $val2[31:0]) : ($sel[1:0] == 2'b11)
+                         ? ($val2[31:0] != 0 ? ($val1[31:0] / $val2[31:0]) : 32'bx) :  32'b0;
+         //$count[31:0] = $reset  ? 0 : (>>1$count + 1);
+   
+   // Assert these to end simulation (before Makerchip cycle limit).
+   *passed = *cyc_cnt > 40;
+   *failed = 1'b0;
+\SV
+	endmodule;
